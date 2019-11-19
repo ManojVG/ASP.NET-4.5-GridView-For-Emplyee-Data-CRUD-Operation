39efdf675728b248785f9d58e33dc64c980c2b0f
Manoj Vishnu Ghuge <MVG9966@GMAIL.COM>
2019-11-19 16:46:59 +0530

    Added Database File & SQL Query File
    Added Database File (.MDF) & SQL Query File (.SQL). You need to change the Connection String according to your Database Server details.

---
+++ Database/CRUDUsingGridView/dbo.Employee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeID]    INT          NOT NULL,
+    [EmployeeName]  VARCHAR (50) NULL,
+    [Gender]        VARCHAR (50) NULL,
+    [DOB]           VARCHAR (50) NULL,
+    [EmailID]       VARCHAR (50) NULL,
+    [ContactNumber] VARCHAR (50) NULL,
+    [Address]       VARCHAR (50) NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
